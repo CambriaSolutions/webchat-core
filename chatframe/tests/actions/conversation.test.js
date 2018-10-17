c1d81337121f3f493c3e74cda49e3d049a281478
Keith Lauher <klauher@cambriasolutions.com>
2018-10-17 12:47:27 -0700

    prettier on files

--- chatframe/tests/actions/conversation.test.js
+++ chatframe/tests/actions/conversation.test.js
@@ -1,97 +1,101 @@
-import * as actions from './../../src/actions/conversation'
+import * as actions from './../../src/actions/conversation';
 
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-import expect from 'expect' 
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import expect from 'expect';
 
-import { Client } from './../../src/conversationClient'
+import { Client } from './../../src/conversationClient';
 
 const options = {
   eventUrl:
     'https://us-central1-dhcs-demo-chat.cloudfunctions.net/eventRequest',
   textUrl: 'https://us-central1-dhcs-demo-chat.cloudfunctions.net/textRequest',
   apiKey: 'XwsfZ0JoM214avhOcgqDZ3S50stTX94l8pnZJNxN',
-  clientName: 'dialogflow'
-}
+  clientName: 'dialogflow',
+};
 
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 const store = mockStore({
-    conversation: {
-        client: new Client(options),
-        clientOptions: options, 
-        clientName:'dialogflow', 
-        messages: [] }
-    })
+  conversation: {
+    client: new Client(options),
+    clientOptions: options,
+    clientName: 'dialogflow',
+    messages: [],
+  },
+});
 
-    const badClientStore = mockStore({
-      conversation: {
-          client: new Client(options),
-          clientOptions: options, 
-          clientName:'test', 
-          messages: [] }
-      })
-
+const badClientStore = mockStore({
+  conversation: {
+    client: new Client(options),
+    clientOptions: options,
+    clientName: 'test',
+    messages: [],
+  },
+});
 
 describe('dialog flow actions', () => {
   afterEach(() => {
-    store.clearActions()
-  })
+    store.clearActions();
+  });
   it('should set client with correct options', () => {
-    store.dispatch(actions.setupClient('dialogflow', options))
-    const storedActions = store.getActions()
-    expect(storedActions[0].type).toEqual('SAVE_CLIENT') 
-  })
-
-  it('should set client with correct options', () => {
-    expect(()=> { 
-      store.dispatch(actions.setupClient('', options))
-    }).toThrow('No conversation provider selected.')  
-  })
+    store.dispatch(actions.setupClient('dialogflow', options));
+    const storedActions = store.getActions();
+    expect(storedActions[0].type).toEqual('SAVE_CLIENT');
+  });
 
   it('should set client with correct options', () => {
-    expect(()=> {
-      store.dispatch(actions.setupClient('dialogflow', {}))
-      const storedActions = store.getActions()
-      expect(storedActions[0].type).toEqual('SAVE_CLIENT') 
-    }).toThrow('Fulfillment URL is required to generate a conversation client')  
-  })
+    expect(() => {
+      store.dispatch(actions.setupClient('', options));
+    }).toThrow('No conversation provider selected.');
+  });
 
   it('should set client with correct options', () => {
-    expect(()=> {
-      store.dispatch(actions.setupClient('low', options))
-      const storedActions = store.getActions()
-      expect(storedActions[0].type).toEqual('SAVE_CLIENT') 
-    }).toThrow('low is not a recognized conversation provider.')  
-  })
+    expect(() => {
+      store.dispatch(actions.setupClient('dialogflow', {}));
+      const storedActions = store.getActions();
+      expect(storedActions[0].type).toEqual('SAVE_CLIENT');
+    }).toThrow('Fulfillment URL is required to generate a conversation client');
+  });
 
-   it('should create user response from text', () => {
-    store.dispatch(actions.createUserResponse('text'))
-    const storedActions = store.getActions()
-    expect(storedActions[0].response.text).toEqual('text') 
-    expect(storedActions[0].type).toEqual('SAVE_USER_RESPONSE')
-    expect(storedActions[1]).toEqual({"type": "HIDE_BUTTON_BAR"})
-    expect(storedActions[2]).toEqual({"type": "INITIATE_LOADING"})   
-   })
-   it('should send message ', () => {
-    const expectedAction = [{"type": "HIDE_BUTTON_BAR"}, {"type": "INITIATE_LOADING"}]
-   store.dispatch(actions.sendMessage('message'))
-   const storedActions = store.getActions()
-   expect(storedActions).toEqual(expectedAction) 
-  })
+  it('should set client with correct options', () => {
+    expect(() => {
+      store.dispatch(actions.setupClient('low', options));
+      const storedActions = store.getActions();
+      expect(storedActions[0].type).toEqual('SAVE_CLIENT');
+    }).toThrow('low is not a recognized conversation provider.');
+  });
 
-   it('should send message ', () => {
-     expect(()=> {
-         badClientStore.dispatch(actions.sendMessage('message'))
-  }).toThrow('test is not a recognized conversation provider.')  
-   })
+  it('should create user response from text', () => {
+    store.dispatch(actions.createUserResponse('text'));
+    const storedActions = store.getActions();
+    expect(storedActions[0].response.text).toEqual('text');
+    expect(storedActions[0].type).toEqual('SAVE_USER_RESPONSE');
+    expect(storedActions[1]).toEqual({ type: 'HIDE_BUTTON_BAR' });
+    expect(storedActions[2]).toEqual({ type: 'INITIATE_LOADING' });
+  });
+  it('should send message ', () => {
+    const expectedAction = [
+      { type: 'HIDE_BUTTON_BAR' },
+      { type: 'INITIATE_LOADING' },
+    ];
+    store.dispatch(actions.sendMessage('message'));
+    const storedActions = store.getActions();
+    expect(storedActions).toEqual(expectedAction);
+  });
+
+  it('should send message ', () => {
+    expect(() => {
+      badClientStore.dispatch(actions.sendMessage('message'));
+    }).toThrow('test is not a recognized conversation provider.');
+  });
   it('should send quick reply', () => {
-    store.dispatch(actions.sendQuickReply('text'))
-  const storedActions = store.getActions()
-  expect(storedActions[0].response.text).toEqual('text') 
-  expect(storedActions[0].type).toEqual('SAVE_USER_RESPONSE')
-  expect(storedActions[1]).toEqual({"type": "HIDE_BUTTON_BAR"})
-  expect(storedActions[2]).toEqual({"type": "INITIATE_LOADING"})   
-  })
-})
+    store.dispatch(actions.sendQuickReply('text'));
+    const storedActions = store.getActions();
+    expect(storedActions[0].response.text).toEqual('text');
+    expect(storedActions[0].type).toEqual('SAVE_USER_RESPONSE');
+    expect(storedActions[1]).toEqual({ type: 'HIDE_BUTTON_BAR' });
+    expect(storedActions[2]).toEqual({ type: 'INITIATE_LOADING' });
+  });
+});
--- chatframe/tests/reducers/errorReducer.test.js
+++ chatframe/tests/reducers/errorReducer.test.js
@@ -1,37 +1,43 @@
-import reducer from '././../../src/reducers/errorReducer'
-import * as types from './../../src/actions/ActionTypes'
+import reducer from '././../../src/reducers/errorReducer';
+import * as types from './../../src/actions/ActionTypes';
 
 describe('reducer', () => {
   it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual(
-    ""
-    )
-  })
+    expect(reducer(undefined, {})).toEqual('');
+  });
   it('should handle SHOW_BUTTON_BAR', () => {
     expect(
       reducer([], {
         type: types.DISPLAY_ERROR,
-        text: 'oops'
-      })).toEqual(undefined)  
+        text: 'oops',
+      })
+    ).toEqual(undefined);
     expect(
-      reducer(
-        [{ text: 'oops test', id: 0}],{ type: types.DISPLAY_ERROR, text: 'oops' }
-        )
-    ).toEqual(undefined)
-  })
+      reducer([{ text: 'oops test', id: 0 }], {
+        type: types.DISPLAY_ERROR,
+        text: 'oops',
+      })
+    ).toEqual(undefined);
+  });
 
   it('should handle HIDE_BUTTON_BAR', () => {
     expect(
       reducer([], {
         type: types.CLEAR_ERROR,
-        text: 'clear it'
-      })).toEqual("")
+        text: 'clear it',
+      })
+    ).toEqual('');
 
     expect(
       reducer(
-      [{ text: 'oops test',
-            id: 0}],{ type: types.CLEAR_ERROR, text: 'Run the tests' }
+        [
+          {
+            text: 'oops test',
+            id: 0,
+          },
+        ],
+        { type: types.CLEAR_ERROR, text: 'Run the tests' }
       )
-    ).toEqual("")
-  })
-})
+    ).toEqual('');
+  });
+});
