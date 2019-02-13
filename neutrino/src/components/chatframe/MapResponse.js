5ffee7be8854e268b33276ab1b791de159f53ec8
Jade Tran <jtran@cambriasolutions.com>
2019-02-13 10:34:01 -0800

    convert pure component to functional component

--- neutrino/src/components/chatframe/MapResponse.js
+++ neutrino/src/components/chatframe/MapResponse.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
@@ -25,63 +25,60 @@ const CardContainer = styled(Card)`
 `
 
 // Maps documentation: https://tomchentw.github.io/react-google-maps
-class MapResponse extends PureComponent {
-  render() {
-    const { data, points, centerCoordinates } = this.props
-    const cardHeight = '300px'
-    const googleMapsUrl = `https://maps.googleapis.com/maps/api/js?key=${
-      this.props.googleMapsKey
-    }&v=3`
-
-    const handleMarkerClick = location => {
-      const url = `https://www.google.com/maps/search/?api=1&query=Google&query_place_id=${
-        location.placeId
-      }`
-      window.open(url, '_blank')
-    }
-
-    const Map = withScriptjs(
-      withGoogleMap(() => (
-        <GoogleMap
-          defaultZoom={6}
-          defaultCenter={centerCoordinates}
-          defaultOptions={{
-            mapTypeControl: false,
-            streetViewControl: false,
-            fullscreenControl: false,
-          }}
-        >
-          {data.map((row, i) => (
-            <Marker
-              key={i}
-              position={{ lat: row.lat, lng: row.lng }}
-              icon={{
-                url: redpin,
-                scaledSize: { width: 20, height: 20 },
-              }}
-              onClick={() => handleMarkerClick(row)}
-            />
-          ))}
-        </GoogleMap>
-      ))
-    )
-
-    return (
-      <CardContainer>
-        <CardContent>
-          <Typography gutterBottom variant='h6'>
-            Office Locations
-          </Typography>
-          <Map
-            googleMapURL={googleMapsUrl}
-            loadingElement={<div style={{ height: `${cardHeight}` }} />}
-            containerElement={<div style={{ height: `${cardHeight}` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-          />
-        </CardContent>
-      </CardContainer>
-    )
+function MapResponse(props) {
+  const { data, centerCoordinates } = props
+  const cardHeight = '300px'
+  const googleMapsUrl = `https://maps.googleapis.com/maps/api/js?key=${
+    props.googleMapsKey
+  }&v=3`
+  const handleMarkerClick = location => {
+    const url = `https://www.google.com/maps/search/?api=1&query=Google&query_place_id=${
+      location.placeId
+    }`
+    window.open(url, '_blank')
   }
+
+  const Map = withScriptjs(
+    withGoogleMap(() => (
+      <GoogleMap
+        defaultZoom={6}
+        defaultCenter={centerCoordinates}
+        defaultOptions={{
+          mapTypeControl: false,
+          streetViewControl: false,
+          fullscreenControl: false,
+        }}
+      >
+        {data.map((row, i) => (
+          <Marker
+            key={i}
+            position={{ lat: row.lat, lng: row.lng }}
+            icon={{
+              url: redpin,
+              scaledSize: { width: 20, height: 20 },
+            }}
+            onClick={() => handleMarkerClick(row)}
+          />
+        ))}
+      </GoogleMap>
+    ))
+  )
+
+  return (
+    <CardContainer>
+      <CardContent>
+        <Typography gutterBottom variant='h6'>
+          Office Locations
+        </Typography>
+        <Map
+          googleMapURL={googleMapsUrl}
+          loadingElement={<div style={{ height: `${cardHeight}` }} />}
+          containerElement={<div style={{ height: `${cardHeight}` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </CardContent>
+    </CardContainer>
+  )
 }
 
 const mapStateToProps = state => {
