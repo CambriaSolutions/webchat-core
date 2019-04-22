cae2b991968a30ea50d33c68bced2f53b20f0a3a
Chris Freeman <cc.freem@yahoo.com>
2019-04-22 16:08:55 -0700

    initial styling edits

--- neutrino/src/components/chatframe/ActivatorButton.js
+++ neutrino/src/components/chatframe/ActivatorButton.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import Fab from '@material-ui/core/Fab'
 import Chat from '@material-ui/icons/Chat'
 import Zoom from '@material-ui/core/Zoom'
+import { withTheme } from '@material-ui/core/styles'
 
 // Redux
 import { connect } from 'react-redux'
@@ -10,23 +11,40 @@ import { showWindow } from './actions/initialization'
 
 const Btn = styled(Fab)`
   && {
-    display: ${p => (p.active ? 'block' : 'none')};
+    display: ${p => (p.active ? 'flex' : 'none')};
     pointer-events: auto;
-    padding-top: 8px;
+    width: auto;
+    height: auto;
+    padding: 8px 15px auto;
+    border-radius: 20px;
+    flex-direction: row;
   }
 `
 
+const TextContainer = styled.div`
+  text-transform: none;
+  padding-right: 10px;
+  color: ${p => p.theme.palette.getContrastText(p.theme.palette.primary.dark)};
+`
+const ChatIcon = styled(Chat)`
+  padding-left: 10px;
+  padding-right: 5px;
+  color: ${p => p.theme.palette.getContrastText(p.theme.palette.primary.dark)};
+`
 class ActivatorButton extends PureComponent {
   render() {
-    const { windowVisible, showWindow } = this.props
+    const { windowVisible, showWindow, theme } = this.props
+    console.log(theme)
     return (
       <Zoom in={!windowVisible} unmountOnExit>
         <Btn
+          id="this"
           color="primary"
           onClick={showWindow}
           active={windowVisible ? 0 : 1}
         >
-          <Chat />
+          <ChatIcon theme={theme} />
+          <TextContainer theme={theme}>Talk to Gen</TextContainer>
         </Btn>
       </Zoom>
     )
@@ -47,7 +65,9 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ActivatorButton)
+export default withTheme()(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(ActivatorButton)
+)
